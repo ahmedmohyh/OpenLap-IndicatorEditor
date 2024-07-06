a51e35d2e647efd604b2492cfe2a024f6e0966e2
AliyahBerutti <148268595+AliyahBerutti@users.noreply.github.com>
2024-07-06 14:37:00 +0200

    [Frontend][#28]
    Added delete button to each selection

--- openlap/frontend/src/pages/Editor/Common/Tag/Tag.jsx
+++ openlap/frontend/src/pages/Editor/Common/Tag/Tag.jsx
@@ -1,5 +1,9 @@
 import { Avatar, Chip, Tooltip } from "@mui/material";
 import {styled} from "@mui/material";
+import IconButton from '@mui/material/IconButton';
+import HighlightOffTwoToneIcon from '@mui/icons-material/HighlightOffTwoTone';
+import CloseRoundedIcon from '@mui/icons-material/CloseRounded';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 const TagStyled = styled(Chip)(() => ({
     marginRight: '4px',
@@ -22,13 +26,28 @@ export default function Tag({ color, step, label, completed, tooltip }) {
             <span style={{ fontSize: '16px' }}>{tooltip}</span>
         } arrow placement="right-start">
             <TagStyled avatar={
-                <AvatarStyled sx={{backgroundColor: `${color}`}}>
+                <AvatarStyled sx={{ backgroundColor: `${color}` }}>
                     <span style={{ color: '#fff' }}>
                         {step + 1}
                     </span>
-                </AvatarStyled>}
-                label={label} variant={completed ? 'filled' : 'outlined'}>
-            </TagStyled>
+                </AvatarStyled>
+            } 
+            label={
+                <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <span>{label}</span>
+                    <IconButton 
+                    size="small" 
+                  
+                    sx={{ marginLeft: '8px', marginRight: '-11px' }} // Negative right margin
+                >
+                    <CancelIcon sx={{ color: '#AEAEAE' }} />
+                </IconButton>
+
+                </div>
+            } 
+            variant={completed ? 'filled' : 'outlined'}
+            />
         </Tooltip>
     );
+    
 }
